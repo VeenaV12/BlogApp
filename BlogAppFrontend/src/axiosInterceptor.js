a83e742d5c9e8e707ad689c52cabb2702c793f34
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-07-03 21:40:01 +0530

    commit

--- BlogAppFrontend/src/axiosInterceptor.js
+++ BlogAppFrontend/src/axiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://blog-app-eta-one.vercel.app/'
 })
 
 axiosInstance.interceptors.request.use((config)=>{
