a52692a8f74aaf310e6f4ac898b733d7dee72864
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-07-03 22:15:47 +0530

    commit again

--- BlogAppBackend/app.js
+++ BlogAppBackend/app.js
@@ -20,7 +20,7 @@ app.use(cors())
 }))*/
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://blog-app-jppb.vercel.app'); // Allow requests from your frontend origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://blog-app-jppb-veena-s-projects.vercel.app/'); // Allow requests from your frontend origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow common HTTP methods
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow common headers
   next();
